bff02377d4a338c7d263a1fddf7f92c1cc173b1a
Ty Cockerham <tcockerham3539@floridapoly.edu>
2026-01-14 19:40:53 +0000

    first attempt

---
+++ print_test.py
@@ -0,0 +1,5 @@
+my_name = "Ty Cockerham"
+print(my_name)
+print("Use 4 spaces per indentation level")
+print("All data in a Python program is represented by objects or by relations between objects")
+print("By taking problem solving and solution I hope to better my understanding of python, become better at working with API's, learn to work as a part of a team to complete a coding project")
